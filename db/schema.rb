49b8170f5b33e4bd1b93a936e441e10a11da7970
Patrick Racenberg <pracenberg@gmail.com>
2015-05-09 13:06:35 -0700

    changed nutritionix_id column in items table to take in string values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509195429) do
+ActiveRecord::Schema.define(version: 20150509200334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20150509195429) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.integer  "nutritionix_id"
+    t.string   "nutritionix_id"
     t.integer  "calories"
     t.integer  "fat"
     t.integer  "protein"
