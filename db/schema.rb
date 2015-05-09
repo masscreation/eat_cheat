10de5c657da5fa285de0aae0b3a67c12738dc576
Patrick Racenberg <pracenberg@gmail.com>
2015-05-09 13:47:24 -0700

    removed unnecessary string column from recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509201024) do
+ActiveRecord::Schema.define(version: 20150509204538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20150509201024) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
-    t.string   "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
