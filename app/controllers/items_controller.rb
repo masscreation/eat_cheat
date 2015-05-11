7484db3e8a26e502bae0e930c960049d3f6ea07e
Patrick Racenberg <pracenberg@gmail.com>
2015-05-11 12:15:49 -0700

    getting info from the API

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,19 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+
+        
+    search = params[:q]
+    parse = "fields=item_name&appId=58809d9f&appKey=f0ee2e843b2e4a910d564ccebfc2c1dd" 
+    if search 
+        resp = Typhoeus.get("https://api.nutritionix.com/v1_1/search/#{search}", params: parse)
+        @items = JSON.parse(resp.body)['hits']
+        # render json: @items
+        puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n #{@items}"
+    else
+        @items = []
+    end
+
   end
 
   def create
