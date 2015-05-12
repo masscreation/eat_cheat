fa35477e8484212bf61d126acb16b8ab45b250a6
Catherine Kazbour <CK@Catherines-MacBook-Pro.local>
2015-05-12 14:58:44 -0700

    show recipe & item via meal: works

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,16 +5,67 @@ class ProfilesController < ApplicationController
 		@meal = Meal.new
 		@recipe_options = Recipe.all.map { |recipe| [recipe.name, recipe.id] }
 		@item_options = Item.all.map { |item| [item.name, item.id] }
+		@items = Item.all
+
+		@today = Time.new
+
+		@food_for_today = []
+
+		#run through recipe.meals
+		current_user.recipes.each do |recipe|
+			recipe.meals.each do |meal|
+				if (meal.time_eaten.day == @today.day) && 
+                  (meal.time_eaten.month == @today.month)
+                  @food_for_today << meal
+              end
+			end
+		end
+
+		@items.each do |item|
+			item.meals.each do |meal|
+				if (meal.time_eaten.day == @today.day) && 
+                  (meal.time_eaten.month == @today.month)
+                  @food_for_today << meal
+              end
+			end
+		end
+
+
+
 
 
 		if !user_signed_in?
 			redirect_to '/users/sign_in'
 		end
-		#display form (toggle) for meals
-
-		#edit in 
-
-
 	end
 
 end
+
+            #   <% @user.recipes.each do |recipe| %>
+
+            
+            #  <% recipe.meals.each do |meal| %>
+            # <% if (meal.time_eaten.day == @today.day) && 
+            #       (meal.time_eaten.month == @today.month) %> 
+
+            # <div style="margin: 15px;"> Recipe:
+            #  <%= meal.recipe.name%>
+            #  <%= meal.time_eaten%>
+            #  <%= meal.time_eaten.day %>
+            #  <%= meal.time_eaten.month %>
+            #  </div>
+            #  <%end%>
+            #  <% end %>   
+            #     <% end %> 
+            
+
+
+            # <% @items.each do |item| %>
+            # <% item.meals.each do |meal| %>
+            # <% if (meal.time_eaten.day == @today.day) && 
+            #       (meal.time_eaten.month == @today.month) %> 
+            # <div style="margin: 15px;"> Item: 
+            #  <%= meal.item.name%>
+            #  <%= meal.time_eaten%> 
+            #               <%= meal.time_eaten.day %>
+            #  <%= meal.time_eaten.month %>
