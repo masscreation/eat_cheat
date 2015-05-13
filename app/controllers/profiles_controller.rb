a62cedeb7be10b5c45118a78b3c5170cad30dc40
Catherine Kazbour <CK@Catherines-MacBook-Pro.local>
2015-05-13 11:45:45 -0700

    add 3 tables, comments, create time-range

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,40 +3,83 @@ class ProfilesController < ApplicationController
 	def index
 		@user = current_user
 		@meal = Meal.new
-		@recipe_options = Recipe.all.map { |recipe| [recipe.name, recipe.id] }
+		@recipe_options = current_user.recipes.map { |recipe| [recipe.name, recipe.id] }
 		@item_options = Item.all.map { |item| [item.name, item.id] }
 		@items = Item.all
 
+            #associate food with time_eaten:
+
 		@today = Time.new
-
 		@food_for_today = []
+            @date_from = params[:@date_from]
+            @date_til = params[:@date_til]
+            @date_range = (@date_from.to_i..@date_til.to_i)
 
-		#run through recipe.meals
-		current_user.recipes.each do |recipe|
-			recipe.meals.each do |meal|
-				if (meal.time_eaten.day == @today.day) && 
-                  (meal.time_eaten.month == @today.month)
-                 
+            #create array for today's recipe data
+		#run through recipe.meals 
 
-                  @food_for_today << {'time_eaten': meal.time_eaten, 'name': meal.recipe.name} 
-              end
-			end
-		end
 
+            current_user.recipes.each do |recipe|
+                  recipe.meals.each do |meal|
+
+                        #select only food eaten today
+                        if (@date_range.cover?(meal.time_eaten.to_i))
+                        #push variables from recipe into array for food eaten today
+                        @food_for_today << {'time_eaten': meal.time_eaten, 'name': meal.recipe.name} 
+                        end
+                  end
+            end
+
+
+
+
+            #create array for today's item data
+            #same thing for items -> run through item.meals
 		@items.each do |item|
 			item.meals.each do |meal|
-				if (meal.time_eaten.day == @today.day) && 
-                  (meal.time_eaten.month == @today.month)
-                  @food_for_today << {'time_eaten': meal.time_eaten, 'name': meal.item.name} 
-              end
+                        #select items from today
+      			if (@date_range.cover?(meal.time_eaten.to_i))
+                        @food_for_today << {'time_eaten': meal.time_eaten, 'name': meal.item.name, 'fat': meal.item.fat, 'protein': meal.item.protein, 'carbs': meal.item.carbs, 'calories': meal.item.calories } 
+                        end
 			end
 		end
+            #test out elements
+            puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n#{@food_for_today}"
 
-		puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n #{@food_for_today}"
+            #Repeat the same as above to specify morning (array with today's mornings recipes)
+            #very undry!! Especially if we have to do this for all times
+            #not working, no error just no data displayed -> meal seems to be nil
+            @food_for_today_morning = []
+
+
+# should be this: in if logic: @food_for_today['time_eaten']
+
+            @items.each do |item|
+                  item.meals.each do |meal|
+                        if (meal.time_eaten.hour > Date.today.beginning_of_day.hour) &&
+                              (meal.time_eaten.hour < Date.today.beginning_of_day.hour + 11.hours)
+                              @food_for_today_morning << @food_for_today
+                        end
+                  end
+            end
+        
+
+
+            #2015-05-13+00%3A00%3A00+-0700&%40date_till=2015-05-13+23%3A59%3A59+-0700
+            
+
+
+            puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n#{@frommer}#{@tiller}"
 
 
 
 
+
+
+
+
+
+            #authorize
 		if !user_signed_in?
 			redirect_to '/users/sign_in'
 		end
@@ -72,3 +115,4 @@ end
             #  <%= meal.time_eaten%> 
             #               <%= meal.time_eaten.day %>
             #  <%= meal.time_eaten.month %>
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ActiveRecord::Base
   has_many :ingredients
   has_many :recipes, through: :ingredients
   has_many :meals
+  belongs_to :user
 end
