1f655f4645020b5d78f94fc1eb633a05b98664c3
MassCreation <dennisb.bishop@gmail.com>
2015-05-13 19:26:24 -0700

    update views, add ingredient dependence to recipe

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,6 +41,11 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @items}
+    end
   end
 
   def destroy
