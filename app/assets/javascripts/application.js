e0d7b034b39374f202c66f3e6b74675aa4e826df
MassCreation <dennisb.bishop@gmail.com>
2015-05-14 16:51:36 -0700

    fixed 'remove ingredient' bug on user recipes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,75 +64,7 @@ $(document).ready(function(){
       }
     });
 
-  // grab the 'recipes-con'
-  var $recipesCon = $("#recipes-con"); 
-
-     console.log($recipesCon.html()); 
-
-    $.get("/users/<%= current_user.id %>/recipes")
-      .done(function (recipes) {
-        console.log("All Recipes:", recipes);
-
-        // append each recipe
-        recipes.forEach(function (recipe) {
-          $recipesCon.append("<div>" + recipe.name + "</div>"); 
-        });
-
-      });
-      // grab the form
-     var $recipeForm = $("#new_recipe"); 
-
-     $recipeForm.on("submit", function (event) {
-      event.preventDefault(); 
-      console.log("Form submitted", $(this).serialize());
-
-      var content = $("#recipe_content").val(); 
-      $.post("/users/:id/recipes", {
-        recipe: {
-          content: content 
-        }
-      }).done(function (createdRecipe) {
-        $recipesCon.append("<div class=\"recipe\" data-id=" + createdRecipe.id + ">" + createdRecipe.content + 
-          "<input type=\"checkbox\" class=\"completed\">" + 
-          "<button class=\"delete\">Delete</button></div>");
-      }); 
-     }); 
-  //  setup a click handler that only
-    //  handle clicks from an element
-    //  with the `.delete` className
-    //  that is inside the $recipesCon
-     $recipesCon.on("click", ".delete", function (event) {
-      alert("I wask clicked!"); 
-      
-      // grab the entire recipe
-      var $recipe = $(this).closest(".recipe"); 
-
-      //send our delete request
-      $.ajax({
-        url: "/users/:id/recipes/" + $recipe.data("id") + ".json",
-        type: "DELETE"
-      }).done(function(){
-        //once we completed the delete
-        $recipe.remove(); 
-      })
-     }); 
-
-     $recipesCon.on("click", ".completed", function () {
-      var $recipe = $(this).closest(".recipe"); 
-
-      $.ajax({
-        url: "/users/:id/recipes" + $recipe.data("id") + ".json", 
-        type: "PATCH",
-        data: {
-          recipe: {
-            completed: this.checked
-          }
-        } 
-      }).done(function (data) {
-        // update the styling of our recipe
-        $recipe.toggleClass("recipe-completed")
-      }) 
-     }); 
+  
 
     
 }); //end ready
